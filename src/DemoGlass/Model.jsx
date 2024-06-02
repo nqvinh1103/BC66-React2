5ee04b30bd90970b7658e7f31015f680163cae53
QVinhDev <vinhnq1103@gmail.com>
2024-06-02 22:13:11 +0700

    update

--- src/DemoGlass/Model.jsx
+++ src/DemoGlass/Model.jsx
@@ -4,12 +4,14 @@ export default class Model extends Component {
   render() {
     return (
       <div className='row'>
-        <div className='contain'>
+        <div className='col-6 contain'>
           <img className='w-25' src="./glass/glassesImage/model.jpg" alt="" />
           <div className='glassImage' style={{ backgroundImage: `url(${this.props.bgImage})` }}></div>
+        </div>
+        <div className="col-6">
+          <img className='w-25' src="./glass/glassesImage/model.jpg" alt="" />
         </div>
       </div>
     )
   }
 }
-  
